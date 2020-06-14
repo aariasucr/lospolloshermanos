d0b7ab768955947f98eeb01ef64a9ebd7fb064da
kamser <keylor.morataya@gmail.com>
2020-06-14 15:30:22 -0600

    Actualizado el archivo defBD.json en la parte de la estructura del post

--- lph/src/app/shared/comment.service.ts
+++ lph/src/app/shared/comment.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private firebaseDatabase: AngularFireDatabase,
+              private firebaseAuth: AngularFireAuth) { }
+
+
 }
