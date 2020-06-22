04d7f085bb730d8a7e5f1f04958d156543ae010f
jimmyacua <jimmyacua@hotmail.com>
2020-06-21 23:27:53 -0600

    pruebas de unidad de post frame component

--- lph/src/app/shared/post-comment.service.spec.ts
+++ lph/src/app/shared/post-comment.service.spec.ts
@@ -5,6 +5,7 @@ import {AngularFireModule} from "@angular/fire";
 import {environment} from "src/environments/environment";
 import {AngularFireDatabaseModule} from "@angular/fire/database";
 import {AngularFireAuthModule} from "@angular/fire/auth";
+import {NgForm, FormsModule} from "@angular/forms";
 
 describe("PostCommentService", () => {
   beforeEach(() =>
