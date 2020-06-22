58ad295396a0f5588f0d320318623f10eddc6408
jimmyacua <jimmyacua@hotmail.com>
2020-06-21 23:19:26 -0600

    arreglos de conflictos de merge

--- lph/src/app/shared/post-comment.service.spec.ts
+++ lph/src/app/shared/post-comment.service.spec.ts
@@ -1,11 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from "@angular/core/testing";
 
-import { PostCommentService } from './post-comment.service';
+import {PostCommentService} from "./post-comment.service";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "src/environments/environment";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {AngularFireAuthModule} from "@angular/fire/auth";
 
-describe('PostCommentService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("PostCommentService", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+      ]
+    })
+  );
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: PostCommentService = TestBed.get(PostCommentService);
     expect(service).toBeTruthy();
   });
